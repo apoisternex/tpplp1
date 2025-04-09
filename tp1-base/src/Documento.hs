6011864e77af0737ac1aab03cdc27df3956ecd79
Crockta <matiasgabrielanastasio@gmail.com>
2025-04-09 18:34:09 -0300

    Cambios a Documento

--- tp1-base/src/Documento.hs
+++ tp1-base/src/Documento.hs
@@ -31,35 +31,24 @@ texto t | '\n' `elem` t = error "El texto no debe contener saltos de línea"
 texto [] = Vacio
 texto t = Texto t Vacio
 
-foldDoc :: ( String -> a -> a ) -> ( Int -> a -> a ) -> a -> Doc -> a
-foldDoc fTexto fLinea z d = case d of
-    Texto s ds -> fTexto s (foldDoc fTexto fLinea z ds)
-    Linea n ds -> fLinea n (foldDoc fTexto fLinea z ds)
-    Vacio -> z
+foldDoc :: (String -> a -> a) -> (Int -> a -> a) -> a -> Doc -> a
+foldDoc fTexto fLinea base docu = case docu of
+    Vacio -> base
+    Texto s ds -> fTexto s (foldDoc fTexto fLinea base ds)
+    Linea n ds -> fLinea n (foldDoc fTexto fLinea base ds)
 
 -- NOTA: Se declara `infixr 6 <+>` para que `d1 <+> d2 <+> d3` sea equivalente a `d1 <+> (d2 <+> d3)`
 -- También permite que expresiones como `texto "a" <+> linea <+> texto "c"` sean válidas sin la necesidad de usar paréntesis.
 infixr 6 <+>
 
-recrDoc :: ( String -> a -> Doc -> a ) -> ( Int -> a -> Doc -> a ) -> a -> Doc -> a
-recrDoc fTexto fLinea z d = case d of
-    Texto s ds -> fTexto s (recrDoc fTexto fLinea z ds) ds
-    Linea n ds -> fLinea n (recrDoc fTexto fLinea z ds) ds
-    Vacio -> z
-
 (<+>) :: Doc -> Doc -> Doc
-(<+>) d1 d2 = recrDoc (\s1 rec ds1-> 
-                          case (ds1, d2) of
-                            (Vacio, Texto s2 ds2) -> (Texto (s1 ++ s2) ds2)
-                            _ -> (Texto s1 rec)
-                       ) (\n rec ds -> Linea n rec) d2 d1
+(<+>) d1 d2 = foldDoc Texto Linea d2 d1
 
 -- concatDoc :: Doc -> Doc -> Doc 
 -- concatDoc d1 d2 = foldDoc (Texto) Linea d2 d1
 
 indentar :: Int -> Doc -> Doc
-indentar i (Linea n ds) = Linea n (foldDoc Texto (\ n rec -> Linea (n + i) rec) Vacio ds)
-indentar i d = foldDoc Texto (\ n rec -> Linea (n + i) rec) Vacio d
+indentar i = foldDoc Texto (\ n rec -> Linea (n + i) rec) Vacio 
 
 mostrar :: Doc -> String
 mostrar = foldDoc (++) ((.) (++) (\i -> "\n"++ replicate i ' ')) ""

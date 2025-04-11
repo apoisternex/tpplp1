ebbc77ba30f8843ead632d5faca97a9d0f047962
Matias <matysuleiman@gmail.com>
2025-04-11 20:41:20 -0300

    casos de Test

--- tp1-base/src/Main.hs
+++ tp1-base/src/Main.hs
@@ -24,7 +24,11 @@ testsEj2 =
   test
     [ vacio <+> vacio ~?= vacio,
       texto "a" <+> texto "b" ~?= texto "ab",
-      (texto "a" <+> linea) <+> texto "b" ~?= texto "a" <+> (linea <+> texto "b")
+      (texto "a" <+> linea) <+> texto "b" ~?= texto "a" <+> (linea <+> texto "b"),
+      --Tests hechos por nosotros
+      (texto "a" <+> texto "b") <+> linea ~?= texto "a" <+> (texto "b" <+> linea),
+      (texto "a" <+> texto "b") <+> texto "c" ~?= texto "a" <+> texto "b" <+> texto "c",
+      texto "a" <+> texto "b" ~?= (<+>) (texto "a") (texto "b")
     ]
 
 testsEj3 :: Test
@@ -33,7 +37,10 @@ testsEj3 =
     [ indentar 2 vacio ~?= vacio,
       indentar 2 (texto "a") ~?= texto "a",
       indentar 2 (texto "a" <+> linea <+> texto "b") ~?= texto "a" <+> indentar 2 (linea <+> texto "b"),
-      indentar 2 (linea <+> texto "a") ~?= indentar 1 (indentar 1 (linea <+> texto "a"))
+      indentar 2 (linea <+> texto "a") ~?= indentar 1 (indentar 1 (linea <+> texto "a")),
+            --Tests hechos por nosotros
+      indentar 0 (linea <+> texto "a") ~?= linea <+> texto "a",
+      indentar 2 (linea <+> indentar 4 linea) ~?= (indentar 2 linea) <+> (indentar 6 linea)
     ]
 
 testsEj4 :: Test
@@ -41,7 +48,9 @@ testsEj4 =
   test
     [ mostrar vacio ~?= "",
       mostrar linea ~?= "\n",
-      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b"
+      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b",
+      -- Tests hechos por nosotros
+      mostrar (indentar 2 (linea <+> texto "a")) ~?= "\n  a"
     ]
 
 pericles, merlina, addams, familias :: PPON
@@ -54,7 +63,9 @@ testsEj6 :: Test
 testsEj6 =
   test
     [ pponObjetoSimple pericles ~?= True,
-      pponObjetoSimple addams ~?= False
+      pponObjetoSimple addams ~?= False,
+      -- Tests hechos por nosotros (aunque usan los objetos definidos por la catedra, pero con estos alcanza para ver los casos que faltan)
+      pponObjetoSimple familias ~?= False -- ¿toma como objeto complejo a algo que tiene adentro un objeto complejo?
     ]
 
 a, b, c :: Doc
@@ -68,19 +79,31 @@ testsEj7 =
     [ mostrar (intercalar (texto ", ") []) ~?= "",
       mostrar (intercalar (texto ", ") [a, b, c]) ~?= "a, b, c",
       mostrar (entreLlaves []) ~?= "{ }",
-      mostrar (entreLlaves [a, b, c]) ~?= "{\n  a,\n  b,\n  c\n}"
+      mostrar (entreLlaves [a, b, c]) ~?= "{\n  a,\n  b,\n  c\n}",
+      --Tests hechos por nosotros
+      mostrar (entreLlaves [a, linea, b, c]) ~?= "{\n  a,\n  \n  ,\n  b,\n  c\n}"
     ]
 
 testsEj8 :: Test
 testsEj8 =
   test
-    [ mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c"
+    [ mostrar (aplanar (a <+> linea <+> b <+> linea <+> c)) ~?= "a b c",
+      -- Tests hechos por nosotros
+      mostrar (aplanar vacio) ~?= "",
+      mostrar (aplanar (indentar 5(linea <+> linea))) ~?= "  ",
+      mostrar (aplanar(texto "a" <+> texto "b")) ~?= "ab",
+      mostrar (aplanar (indentar 2(a <+> linea <+> b <+> linea <+> c))) ~?= "a b c"
     ]
-
+objetoMixto :: PPON
+objetoMixto = ObjetoPP [("familias",familias),("saludo",TextoPP "hola"),("clon de Pericles",pericles)]
 testsEj9 :: Test
 testsEj9 =
   test
     [ mostrar (pponADoc pericles) ~?= "{ \"nombre\": \"Pericles\", \"edad\": 30 }",
       mostrar (pponADoc addams) ~?= "{\n  \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n  \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n}",
-      mostrar (pponADoc familias) ~?= "{\n  \"Addams\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  }\n}"
+      mostrar (pponADoc familias) ~?= "{\n  \"Addams\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  }\n}",
+      --Tests hechos por nosotros
+      mostrar (pponADoc (TextoPP "hola")) ~?= "\"hola\"",
+      mostrar (pponADoc (IntPP 6)) ~?= "6",
+      mostrar (pponADoc objetoMixto) ~?= "{\n  \"familias\": {\n    \"Addams\": {\n      \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n      \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n    }\n  },\n  \"saludo\": \"hola\",\n  \"clon de Pericles\": { \"nombre\": \"Pericles\", \"edad\": 30 }\n}"
     ]
